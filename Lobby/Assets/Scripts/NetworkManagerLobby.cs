f8ef449bac877f9c5460c9ec03947a03beb1edb9
ChristopherOrtiz69 <100314938+AnaRB29@users.noreply.github.com>
2024-05-07 16:26:39 -0600

    proyecto

---
+++ Lobby/Assets/Scripts/NetworkManagerLobby.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using System;
+using System.Linq;
+using UnityEngine.SceneManagement;
+
+public class NetworkManagerLobby : NetworkManager
+{
+    [Scene]
+    [SerializeField] private string menuScene = string.Empty;
+
+    [Header("Room")]
+    [SerializeField] private NetworkRoomPlayerLobby roomPlayerPrefab = null;
+
+    public static event Action OnClientConnected;
+    public static event Action OnClientDisconnected;
+
+    public override void OnStartServer() => spawnPrefabs = Resources.LoadAll<GameObject>("SpawanablePrefabs").ToList();
+
+    public override void OnStartClient()
+    {
+        var spawanablePrefabs = Resources.LoadAll<GameObject>("SpawanblePrefabs");
+        foreach (var prefab in spawanablePrefabs)
+        {
+            NetworkClient.RegisterPrefab(prefab);
+        }
+    }
+
+    public override void OnClientConnect()
+    {
+        OnClientConnected?.Invoke();
+    }
+
+    public override void OnClientDisconnect()
+    {
+        OnClientDisconnected?.Invoke();
+    }
+
+    public override void OnServerConnect(NetworkConnectionToClient conn)
+    {
+        base.OnServerConnect(conn);
+        if (numPlayers >= maxConnections)
+        {
+            conn.Disconnect();
+            return;
+        }
+
+        if (SceneManager.GetActiveScene().name != menuScene)
+        {
+            conn.Disconnect();
+            return;
+        }
+    }
+
+    public override void OnServerAddPlayer(NetworkConnectionToClient conn)
+    {
+        base.OnServerAddPlayer(conn);
+        if (SceneManager.GetActiveScene().name == menuScene)
+        {
+            NetworkRoomPlayerLobby roomPlayerInstance = Instantiate(roomPlayerPrefab);
+            NetworkServer.AddPlayerForConnection(conn, roomPlayerInstance.gameObject);
+        }
+    }
+
+
+
+
+
+
+
+}
